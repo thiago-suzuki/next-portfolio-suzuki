a08ad11e2a44d9e588a7bb35659502e6510f81d6
Thiago Suzuki <thiagoikenaga@gmail.com>
2025-07-11 02:24:38 -0300

    fix: fix height

--- src/components/Pages/About/index.tsx
+++ src/components/Pages/About/index.tsx
@@ -70,7 +70,7 @@ export function About() {
                             {/* Figma */}
                             <div className="flex flex-col items-center">
                                 <div className="relative w-24 h-24">
-                                    <div className="w-24 h-48 rounded-full border-4 border-primary-500"></div>
+                                    <div className="w-24 h-24 rounded-full border-4 border-primary-500"></div>
                                     <div className="absolute inset-0 flex items-center justify-center">
                                         <span className="text-2xl">ƒƒ</span>
                                     </div>
