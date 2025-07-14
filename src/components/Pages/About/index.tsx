882c01d79106dc97fceaf2b5428c409e87e7051d
Thiago Suzuki <thiagoikenaga@gmail.com>
2025-07-14 01:21:09 -0300

    fix: fix size about

--- src/components/Pages/About/index.tsx
+++ src/components/Pages/About/index.tsx
@@ -26,7 +26,7 @@ export function About() {
               },
             }}
         >
-            <div className="text-white">
+            <div className="text-white pb-12">
                 <div className="container mx-auto px-4">
                     <div className="grid grid-cols-1 gap-8">
                         <div className="text-center">
